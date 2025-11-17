3a755ae102b96d821c5d9c4c51b45e9dbc65f031
hekang1992 <hekang19921111@hotmail.com>
2025-11-17 18:00:52 +0800

    add:新增登陆页面

---
+++ wintaocloud/Login/LoginFootView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginFootView.swift
+//  wintaocloud
+//
+//  Created by hekang on 2025/11/17.
+//
+
+import UIKit
+
+class LoginFootView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ wintaocloud/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  wintaocloud
+//
+//  Created by hekang on 2025/11/17.
+//
+
+import UIKit
+
+class LoginView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
